2eb08609a7cecd5f4cab851ced84d64e546e1d88
Igor <leonidlee777@gmail.com>
2021-12-25 11:52:48 +0200

    additional loading of characters

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -2,6 +2,7 @@ class MarvelService {
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
    
     _apiKey = 'apikey=03679cda5fde628be86231cfcb54fd6c';
+    _baseOffset = 210;
 
 
     getResource = async (url) => {
@@ -14,8 +15,8 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = async () => {
-        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters = async (offset = this._baseOffset) => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
         return res.data.results.map(this._transformCharacter);
     }
 
